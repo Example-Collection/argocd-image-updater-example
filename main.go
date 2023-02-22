d062ca9024c5bf16684b7faec7413a31048b8017
sang-w0o <robbyra@gmail.com>
2023-02-22 13:51:31 +0900

    Test if ArgoCD image updater respects git commit

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		_, _ = w.Write([]byte(`{"message": "server is running, version 0.0.3"}`))
+		_, _ = w.Write([]byte(`{"message": "server is running, version 0.0.4"}`))
 	})
 	_ = http.ListenAndServe(":8080", nil)
 }
